ef55ec06761d3b8a3b5ac362757fb31e9fe87cca
sjriddle <samuel.riddle1@gmail.com>
2021-12-09 14:04:18 -0700

    Removed BSC from default list

--- src/buildList.js
+++ src/buildList.js
@@ -1,11 +1,11 @@
 const { version } = require("../package.json");
-/*const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
-const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");*/
-const bscMainnet = require("./tokens/bsc-mainnet.json");
-const bscTestnet = require("./tokens/bsc-testnet.json");
+// const mainnet = require("./tokens/mainnet.json");
+// const ropsten = require("./tokens/ropsten.json");
+// const rinkeby = require("./tokens/rinkeby.json");
+// const goerli = require("./tokens/goerli.json");
+// const kovan = require("./tokens/kovan.json");
+// const bscMainnet = require("./tokens/bsc-mainnet.json");
+// const bscTestnet = require("./tokens/bsc-testnet.json");
 const harmonyMainnet = require("./tokens/harmony-mainnet.json");
 const harmonyTestnet = require("./tokens/harmony-testnet.json");
 
@@ -22,7 +22,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "https://s3.us-west-2.amazonaws.com/farmersonly.fi/FOX-Logo.png",
     keywords: ["foxswap", "default"],
-    tokens: [...bscMainnet, ...bscTestnet, ...harmonyMainnet, ...harmonyTestnet]
+    tokens: [...harmonyMainnet, ...harmonyTestnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
