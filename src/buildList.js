de7e81b5381db5d15e4d7dd2bfcb98c3723b1791
Riddle <samuel.riddle1@gmail.com>
2022-05-26 13:59:46 -0600

    Removing test tokens on harmony

--- src/buildList.js
+++ src/buildList.js
@@ -1,13 +1,5 @@
 const { version } = require("../package.json");
-// const mainnet = require("./tokens/mainnet.json");
-// const ropsten = require("./tokens/ropsten.json");
-// const rinkeby = require("./tokens/rinkeby.json");
-// const goerli = require("./tokens/goerli.json");
-// const kovan = require("./tokens/kovan.json");
-// const bscMainnet = require("./tokens/bsc-mainnet.json");
-// const bscTestnet = require("./tokens/bsc-testnet.json");
 const harmonyMainnet = require("./tokens/harmony-mainnet.json");
-const harmonyTestnet = require("./tokens/harmony-testnet.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
@@ -22,7 +14,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "https://s3.us-west-2.amazonaws.com/farmersonly.fi/FoxSwapLogos/foxswap-circle_05.svg",
     keywords: ["foxswap", "default"],
-    tokens: [...harmonyMainnet, ...harmonyTestnet]
+    tokens: [...harmonyMainnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
